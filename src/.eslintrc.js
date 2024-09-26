7eab6913c926b32676f621d12e5bd388765706c1
Kyavawa Rashidah <kyavawa.rashidah@student.mak.ac.ug>
2024-09-26 21:54:15 +0600

    updated

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,11 +1,27 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
+const pluginReact = require("eslint-plugin-react");
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended", // Use the recommended configuration for React
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detects the react version
+    },
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
