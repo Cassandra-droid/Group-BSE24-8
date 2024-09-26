8ddd914f7518a86dc68057f230024c4dafd293cd
Kyavawa Rashidah <kyavawa.rashidah@student.mak.ac.ug>
2024-09-26 17:08:09 +0600

    added new file

---
+++ src/setTest.js
@@ -0,0 +1,11 @@
+// setupTests.js
+Object.defineProperty(window, 'localStorage', {
+    value: {
+      getItem: jest.fn(() => null),
+      setItem: jest.fn(),
+      removeItem: jest.fn(),
+      clear: jest.fn(),
+    },
+    writable: true,
+  });
+  
