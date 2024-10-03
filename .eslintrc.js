911b4df646bea0cbd0950c4839f7c482c0bf3da9
Britney5pearl <108064189+Britney5pearl@users.noreply.github.com>
2024-10-03 09:39:33 -0800

    delete.eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,1 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    // Add custom linting rules here
-  },
-};
 
