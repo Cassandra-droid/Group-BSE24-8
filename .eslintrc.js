f388ecc8effedf08336f58130be07b77c836a846
Britney5pearl <108064189+Britney5pearl@users.noreply.github.com>
2024-10-03 10:39:08 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    // Add custom linting rules here
+  },
+};
 
