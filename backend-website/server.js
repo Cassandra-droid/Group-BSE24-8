ace765ab7f465f2ac53936360ee66db41214149f
Kyavawa Rashidah <kyavawa.rashidah@student.mak.ac.ug>
2024-10-13 11:25:44 +0300

    logging mechanism

--- backend-website/server.js
+++ backend-website/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js'; // Note the .js extension for ESM
+import logRoutes from './routes/logRoutes.js';
 import dotenv from 'dotenv';
 
 dotenv.config();//load environment variables from .env file
@@ -17,6 +18,7 @@ app.use(bodyParser.json());
 // Authentication routes
 app.use('/api/auth', authRoutes);
 
+app.use('/api/logs', logRoutes); //
 // Start the server
 const PORT = 5000;
 app.listen(PORT, () => {
